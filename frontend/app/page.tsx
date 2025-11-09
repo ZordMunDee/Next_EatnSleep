3b4b86d97b732884a0a11482002fcb5f5ed4e076
zeroznice <zeroznice.nice@gmail.com>
2025-11-09 23:03:56 +0700

    Navbar

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button variant="outline">Submit</Button>
+    </div>
+  );
+};
+export default HomePage;
---
+++ frontend/components/Navbar/Search.tsx
@@ -0,0 +1,6 @@
+import { Input } from "../ui/input";
+
+const Search = () => {
+  return <Input type="Text" placeholder="Search" className="max-w-xs" />;
+};
+export default Search;
