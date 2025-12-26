66ce41ee481775fae02ab1ce76d745b110356e37
ZordMunDee <zeroznice.nice@gmail.com>
2025-12-26 23:07:15 +0700

    form

--- frontend/app/Providers.tsx
+++ frontend/app/Providers.tsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "./theme-provider";
 import { Toaster } from "sonner";
 
+
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
---
+++ frontend/app/profile/create/page.tsx
@@ -0,0 +1,14 @@
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
+
+
+
+const CreateProfilePage = () => {
+  return (
+    <section>CreateProfilePage</section>
+  )
+}
+
+export default CreateProfilePage
