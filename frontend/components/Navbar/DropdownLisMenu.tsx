220e5f75ceb9647e3da8f463192423d301857af3
ZordMunDee <zeroznice.nice@gmail.com>
2025-12-26 16:03:30 +0700

    Login&Logout

--- frontend/components/Navbar/DropdownLisMenu.tsx
+++ frontend/components/Navbar/DropdownLisMenu.tsx
@@ -17,6 +17,8 @@ import { Button } from "../ui/button";
 import UserIcon from "./Usericon";
 import Link from "next/link";
 import { links } from "@/utils/links";
+import SignOutLinks from "./SignOutLinks";
+import { SignedIn, SignedOut, SignInButton, SignUp, SignUpButton } from "@clerk/nextjs";
 
 const DropdownLisMenu = () => {
   return (
@@ -31,6 +33,23 @@ const DropdownLisMenu = () => {
         <DropdownMenuLabel>My Account</DropdownMenuLabel>
         <DropdownMenuSeparator />
 
+      {/* ล็อคเอ้าแล้ว */}
+      <SignedOut>
+        <DropdownMenuItem>
+          <SignInButton mode="modal">
+            <button>Login</button>
+          </SignInButton>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <SignUpButton mode="modal">
+            <button>Register</button>
+          </SignUpButton>
+        </DropdownMenuItem>
+      </SignedOut>
+      
+      
+      {/* ล็อคอินแล้ว */}
+      <SignedIn>
         {links.map((item, index) => {
           return (
             <DropdownMenuItem key={index}>
@@ -38,6 +57,13 @@ const DropdownLisMenu = () => {
             </DropdownMenuItem>
           );
         })}
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <SignOutLinks />
+        </DropdownMenuItem>
+      </SignedIn>
+
+        
       </DropdownMenuContent>
     </DropdownMenu>
   );
